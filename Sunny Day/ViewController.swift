b5d3ff7a629debb87db8e2c709f82e07b6ebf3cc
Heiner Bruss <heiner.bruss@web.de>
2020-09-15 16:45:33 +0200

    Initial Commit

---
+++ Sunny Day/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunny Day
+//
+//  Created by Heiner Bruß on 15.09.20.
+//  Copyright © 2020 Heiner Bruß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
